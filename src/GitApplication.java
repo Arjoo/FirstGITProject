6c74a09430e65916a06f59686ce50ce44510fea3
arjookumar <2009arjoo@gmail.com>
2018-02-04 00:17:22 +0530

    ammend
    revering commit

--- src/GitApplication.java
+++ src/GitApplication.java
@@ -9,6 +9,8 @@ public class GitApplication {
 		System.out.println("Diff");
 		System.out.println("1");
 		System.out.println("2");
+		System.out.println("ammend");
+		System.out.println("ammend check");
 	
 	}
 
